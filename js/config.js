08b6727ecd8ef85bbd488d5c85b1722b7cb7f43f
smyrlcam <62360602+smyrlcam@users.noreply.github.com>
2021-11-20 16:14:56 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_C3s6uAumF', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2hgpiqs8qsmppt8suoj8clbjmb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
